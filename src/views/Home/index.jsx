1643ec0cebb9176872d2fca5cfbc171ef6871a27
Wes Moberly <wesm87@gmail.com>
2017-07-11 01:48:53 -0400

    Put `Home View` text inside `<Container>`

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,5 +1,7 @@
 
 import React, { PureComponent } from 'react'
+import { Container } from 'semantic-ui-react'
+
 import FriendsList from 'components/FriendsList'
 
 export default class HomeView extends PureComponent {
@@ -7,7 +9,9 @@ export default class HomeView extends PureComponent {
   render() {
     return (
       <div>
-        <h1>Home View</h1>
+        <Container text>
+          <h1>Home View</h1>
+        </Container>
         <FriendsList />
       </div>
     )
