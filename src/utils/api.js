a649ee52f8c0b4248090e07171abc89fd034974d
Wes Moberly <wesm87@gmail.com>
2017-07-11 00:14:26 -0400

    Add default value for `SGG_API_APP_NAME`

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 
-const { SGG_API_APP_NAME } = process.env
+const { SGG_API_APP_NAME = 'social-gaming-guild-api' } = process.env
 
 export function getApiUrl() {
   return `https://${SGG_API_APP_NAME}.herokuapp.com`
