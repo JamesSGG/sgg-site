0621abbc5cf306d6bd64d8a10374a82541ee79c1
Wes Moberly <wesm87@gmail.com>
2017-08-07 02:28:36 -0400

    Move user profile info into separate component
    - Change "Games Played" dropdown to a multi-select
    - "Gamer Tags" input now has an "Add tag" button

--- src/views/UserProfile/index.jsx
+++ src/views/UserProfile/index.jsx
@@ -1,100 +1,25 @@
 
 import React, { Component } from 'react'
-import { graphql } from 'react-apollo'
-import { Container, Grid, Item, Header, Dropdown, Input } from 'semantic-ui-react'
+import { Container, Header } from 'semantic-ui-react'
 
-import type { DefaultChildProps } from 'react-apollo'
-
-// $FlowIgnore
-import Q_USER from 'data/q-user.graphql'
+import UserInfo from './UserInfo'
 
 
-type Props = DefaultChildProps<*, *>;
+type Props = {};
 
 
-const { localStorage } = window
-
-const currentUserId = localStorage.getItem('userId')
-
-@graphql(Q_USER, {
-  skip: !currentUserId,
-  options: {
-    variables: {
-      id: currentUserId,
-    },
-  },
-})
 export default class UserProfileView extends Component {
 
   props: Props
 
   render() {
-    const { data, loading, error } = this.props
-
-    if (loading || error) {
-      return null
-    }
-
-    const { user = {} } = data
-    const { displayName, imageUrl } = user
-
-    const gamesPlayedOptions = [
-      {
-        text: 'Overwatch',
-        value: 'overwatch',
-      },
-      {
-        text: 'DotA 2',
-        value: 'dota2',
-      },
-      {
-        text: 'League of Legends',
-        value: 'league-of-legends',
-      },
-      {
-        text: 'Rainbow 6: Siege',
-        value: 'rainbow-6-siege',
-      },
-    ]
-
     return (
-      <div>
-        <Container text>
-          <Header as="h1" textAlign="center">
-            Gamer Profile
-          </Header>
-          <Item.Group>
-            <Item>
-              <Item.Image size="tiny" src={imageUrl} />
-
-              <Item.Content>
-                <Item.Header>
-                  {displayName}
-                </Item.Header>
-                <Item.Description>
-                  <Grid columns={2}>
-                    <Grid.Column>
-                      <p>Games Played</p>
-                      <Dropdown
-                        placeholder="Select a game"
-                        options={gamesPlayedOptions}
-                      />
-                    </Grid.Column>
-                    <Grid.Column>
-                      <p>Gamer Tags</p>
-                      <Input
-                        icon="tags"
-                        iconPosition="left"
-                        placeholder="Add gamer tag"
-                      />
-                    </Grid.Column>
-                  </Grid>
-                </Item.Description>
-              </Item.Content>
-            </Item>
-          </Item.Group>
-        </Container>
-      </div>
+      <Container text>
+        <Header as="h1" textAlign="center">
+          Gamer Profile
+        </Header>
+        <UserInfo />
+      </Container>
     )
   }
 }
